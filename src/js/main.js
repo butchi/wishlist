12a324cd4d19cf36f0b814a1bf84d6ccd14c7847
IWABUCHI Yu(u)ki <k38647+reg@gmail.com>
2016-06-28 18:33:43 +0900

    construct main

--- src/js/main.js
+++ src/js/main.js
@@ -1,24 +1,30 @@
 console.log('Hello, world!');
 
+class Main {
+  constructor() {
+    const $body = $('body');
+
+    const $list = $('.list');
+    var $item = $list.find('.item');
+
+    $list.on('click', '.item', (evt) => {
+      const $target = $(evt.target);
+      $target.closest('.item').attr('data-clicked', '1');
+    });
+
+    $(window).on('focus', (evt) => {
+      $item.filter('[data-clicked]').each((i, itm) => {
+        let $itm = $(itm);
+        let compiled = $itm.attr('data-compiled');
+        $itm.find('.link').text(compiled);
+      });
+    });
+  }
+}
+
 window.licker = window.licker || {};
 ((ns) => {
-  const $body = $('body');
-
-  const $list = $('.list');
-  var $item = $list.find('.item');
-
-  $list.on('click', '.item', (evt) => {
-    const $target = $(evt.target);
-    $target.closest('.item').attr('data-clicked', '1');
-  });
-
-  $(window).on('focus', (evt) => {
-    $item.filter('[data-clicked]').each((i, itm) => {
-      let $itm = $(itm);
-      let compiled = $itm.attr('data-compiled');
-      $itm.find('.link').text(compiled);
-    });
-  });
+  ns.main = new Main();
 })(window.licker);
 
 console.log('Thanks, world!');
